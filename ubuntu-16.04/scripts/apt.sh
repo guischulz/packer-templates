bc5d32b5d80ec2cda5747632dd85f918e8454d6c
Guido Schulz <guido.schulz@mettenmeier.de>
2018-01-12 19:48:22 +0100

    Add apt package update-notifier-common

--- ubuntu-16.04/scripts/apt.sh
+++ ubuntu-16.04/scripts/apt.sh
@@ -5,7 +5,7 @@
 declare -x DEBIAN_FRONTEND="noninteractive"
 
 apt-get update
-apt-get -yq install unattended-upgrades
+apt-get -yq install update-notifier-common unattended-upgrades
 unattended-upgrade
 
 systemctl stop sshd.service
